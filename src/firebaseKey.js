ff6274c0944d0ce3b45043d2e46c70533f9c24f7
David <davidsalem37@gmail.com>
2024-02-13 02:54:13 +0200

    fixing bugs

---
+++ src/firebaseKey.js
@@ -0,0 +1,7 @@
+const firebaseKey = {
+  FIREBASE_KEY: "AIzaSyDe-Vowqs0i4vHjQHKYj-B-ny7_4H6-MhE",
+  FIREBASE_AUTH_DOMAIN: "social-media-app-2fd47.firebaseapp.com",
+  FIREBASE_PROJECT_ID: "social-media-app-2fd47",
+};
+
+export default firebaseKey;
